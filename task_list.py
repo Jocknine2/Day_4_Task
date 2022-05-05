535a2219b296da4f5d773e6b7dfb0f700704f456
Nick Jones <nickj_24@hotmail.com>
2022-05-05 16:13:17 +0100

    completed task 1, updates for task 2

--- task_list.py
+++ task_list.py
@@ -1,3 +1,7 @@
+from start_code.app import *
+from start_code.output import *
+
+
 tasks = [
     {"description": "Wash Dishes", "completed": False, "time_taken": 10},
     {"description": "Clean Windows", "completed": False, "time_taken": 15},
@@ -12,7 +16,7 @@ tasks = [
 def get_uncompleted_tasks(list):
     found = []
 
-    for task in tasks:
+    for task in list:
         if task["completed"] == False:
             found.append(task)
 
@@ -25,7 +29,7 @@ print(get_uncompleted_tasks(tasks))
 def get_completed_tasks(list):
     found = []
 
-    for task in tasks:
+    for task in list:
         if task["completed"] == True:
             found.append(task)
 
@@ -39,7 +43,7 @@ print("_______________________________________")
 ## Get tasks where time_taken is at least a given time
 def get_tasks_taking_at_least(list, time):
 
-    for times in tasks:
+    for times in list:
         if times["time_taken"] == time:
 
             return times
@@ -52,7 +56,7 @@ print("_______________________________________")
 ## Find a task with a given description
 def get_task_with_description(list, description):
 
-    for task in tasks:
+    for task in list:
         if task["description"] == description:
 
             return task
@@ -65,79 +69,14 @@ print(get_task_with_description(tasks, "Make Dinner"))
 
 ## Get a list of tasks by status
 def get_tasks_by_status(list, status):
-    pass
+
+    status_list = []
+
+    for stat in list:
+        if stat["completed"] == status:
+            status_list.append(stat)
+
+    return stat and print(status_list)
 
 
-def mark_task_complete(task):
-    task["completed"] = True
-
-
-def create_task(description, time_taken):
-    task = {}
-    task["description"] = description
-    task["completed"] = False
-    task["time_taken"] = time_taken
-
-    return task
-
-
-def add_to_list(list, task):
-    list.append(task)
-
-
-def print_task(task):
-    print(f'Description: { task["description"] }')
-    print(f'Status: { "Completed" if task["completed"] else "Incomplete"}')
-    print(f'Time Taken: {task["time_taken"]} mins')
-
-
-def print_list(list):
-    for task in list:
-        print_task(task)
-
-
-def print_menu():
-    print("Options:")
-    print("1: Display All Tasks")
-    print("2: Get Uncompleted Tasks")
-    print("3: Get Completed Tasks")
-    print("4: Mark Task as Complete")
-    print("5: Get Tasks Which Take Longer Than a Given Time")
-    print("6: Find Task by Description")
-    print("7: Add a new Task to list")
-    print("M or m: Display this menu")
-    print("Q or q: Quit")
-
-
-while True:
-    print_menu()
-    option = input("Select an option 1, 2, 3, 4, 5, display (m)enu or (q)uit: ")
-    if option.lower() == "q":
-        break
-    if option == "1":
-        print_list(tasks)
-    elif option == "2":
-        print_list(get_uncompleted_tasks(tasks))
-    elif option == "3":
-        print_list(get_completed_tasks(tasks))
-    elif option == "4":
-        description = input("Enter task description to search for: ")
-        task = get_task_with_description(tasks, description)
-        if task is not None:
-            mark_task_complete(task)
-            print("Task marked complete")
-        else:
-            print("Task not found")
-    elif option == "5":
-        time = int(input("Enter task duration: "))
-        print_list(get_tasks_taking_at_least(tasks, time))
-    elif option == "6":
-        description = input("Enter task description to search for: ")
-        print(get_task_with_description(tasks, description))
-    elif option == "7":
-        description = input("Enter description: ")
-        time_taken = int(input("Enter time taken: "))
-        task = create_task(description, time_taken)
-        tasks.append(task)
-    else:
-        print("Invalid Input - choose another option")
+print(get_tasks_by_status(tasks, True))
